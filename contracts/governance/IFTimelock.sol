c632841930ac3963cd3f85a0338d1056ec774f9e
trung <trungtt1981@gmail.com>
2023-10-29 17:20:57 +0100

    Add to Timelock the func for transferring erc20 and NFT tokens

--- contracts/governance/IFTimelock.sol
+++ contracts/governance/IFTimelock.sol
@@ -2,10 +2,19 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/governance/TimelockController.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
-// This Timelock contract holds funds like native SMR
+// This Timelock contract holds funds including native gas token, erc20 and NFT.
 // The funds transfer can only be via proposal execution
 contract IFTimelock is TimelockController {
+    modifier onlyThisContract() {
+        _checkCaller(msg.sender);
+        _;
+    }
+
     constructor(
         uint256 minDelay,
         address[] memory proposers,
@@ -13,15 +22,63 @@ contract IFTimelock is TimelockController {
         address admin
     ) TimelockController(minDelay, proposers, executors, admin) {}
 
-    // Transfer native gas token from the caller to the specified recipient.
-    // The caller must be the timelock itself.
-    // Means that, it must go via proposal execution.
-    function transferNative(address payable recipient) external payable {
-        address sender = _msgSender();
-        if (sender != address(this)) {
-            revert TimelockUnauthorizedCaller(sender);
+    function _checkCaller(address callerAddr) internal view {
+        if (callerAddr != address(this)) {
+            revert TimelockUnauthorizedCaller(callerAddr);
         }
+    }
 
+    // Transfer native gas token from the caller, which must be the Timelock itself,
+    // to the specified recipient via the Timelock-based proposal execution.
+    function transferNative(
+        address payable recipient
+    ) external payable onlyThisContract {
+        require(recipient != address(0), "Invalid recipient address");
         recipient.transfer(msg.value);
+    }
+
+    // Transfer erc20 token from the caller, which must be the Timelock itself,
+    // to the specified recipient via the Timelock-based proposal execution.
+    function transferErc20(
+        address erc20TokenAddress,
+        address recipient,
+        uint256 amount
+    ) external onlyThisContract {
+        require(recipient != address(0), "Invalid recipient address");
+        require(erc20TokenAddress != address(0), "Invalid erc20 token address");
+        require(amount > 0, "Invalid erc20 token amount");
+
+        IERC20(erc20TokenAddress).transfer(recipient, amount);
+    }
+
+    // Transfer NFT token from the caller, which must be the Timelock itself,
+    // to the specified recipient via the Timelock-based proposal execution.
+    function transferNFT(
+        address nftTokenAddress,
+        address recipient,
+        uint256 tokenId,
+        uint256 amount
+    ) external onlyThisContract {
+        require(recipient != address(0), "Invalid recipient address");
+        require(nftTokenAddress != address(0), "Invalid NFT token address");
+        require(amount > 0, "Invalid NFT token amount");
+
+        IERC165 tokenContract = IERC165(nftTokenAddress);
+
+        if (tokenContract.supportsInterface(type(IERC1155).interfaceId)) {
+            IERC1155(nftTokenAddress).safeTransferFrom(
+                address(this),
+                recipient,
+                tokenId,
+                amount,
+                ""
+            );
+        } else {
+            IERC721(nftTokenAddress).safeTransferFrom(
+                address(this),
+                recipient,
+                tokenId
+            );
+        }
     }
 }
